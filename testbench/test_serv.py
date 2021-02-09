7a3eec4cdaf5203b4a583d666c28bae2a72b51a9
yitiandelan <yitiandelan@outlook.com>
2021-02-09 20:53:54 +0800

    test: update testbench

--- testbench/test_serv.py
+++ testbench/test_serv.py
@@ -3,6 +3,8 @@ import re
 from cocotb import test as testbench
 from cocotb import triggers, clock, fork
 from cocotb_test import simulator
+from tempfile import TemporaryDirectory
+from struct import unpack
 
 
 async def set_reset(clk, rst, delay=10, active=1):
@@ -16,12 +18,35 @@ async def set_regs(clk, obj, regs: list, value=0):
         t.value = value
 
 
-async def exec_rom(clk, ra, rr, rd, rv, data=list):
+async def exec_rom(clk, ra, rr, rd, rv, data: dict):
     while True:
         await triggers.RisingEdge(clk)
         raddr, rread, rdatavalid = ra.value, rr.value, rv.value
         rv.value = 0 if not rread else 1 ^ rdatavalid
         rd.value = 0 if not rread else data[raddr//4]
+
+
+def build_target(files, target="verilog", cc="riscv64-elf-gcc", objcopy="riscv64-elf-objcopy"):
+    with TemporaryDirectory() as d:
+        t1, t2, t3, t4 = "", "", "{}/out.elf".format(d), "{}/out.mem".format(d)
+        for k, v in files.items():
+            with open("{}/{}".format(d, k), "w") as fp:
+                fp.write(v)
+            t1 = "{}/{}".format(d, k) if k.endswith(".ld") else t1
+            t2 = "{}/{}".format(d, k) if k.endswith(".s") else t2
+
+        command = "{} -nostartfiles -march=rv32i -mabi=ilp32".format(cc)
+        command += " -T{} -o {} {}".format(t1, t3, t2)
+        command += " && {}".format(objcopy)
+        command += " --target={} {} {}".format(target, t3, t4)
+        os.system(command)
+
+        assert os.path.isfile(t4)
+
+        with open(t4, "r") as fp:
+            _ret = fp.read()
+
+    return _ret
 
 
 @testbench(timeout_time=2,
@@ -31,8 +56,39 @@ async def serv_exec_blinky(dut):
     fork(clock.Clock(dut.clk, 10, "ns").start())
     fork(set_reset(dut.clk, dut.i_rst, delay=3))
 
-    mem = {k: v for k, v in enumerate(
-        [0x40000537,  0x00050513, 0x00100337, 0x00550023])}
+    files = {}
+    files["blink.s"] = '''
+        .globl _start
+        _start:
+            lui a0, %hi(0x40000000)
+            addi a0, a0, %lo(0x40000000)
+            li t1, 0x100000
+        bl1:
+            sb t0, 0(a0)
+            xori t0, t0, 1
+            and t2, zero, zero
+        time1:
+            addi t2, t2, 1
+            bne t1, t2, time1
+            j bl1
+        '''
+    files["link.ld"] = '''
+        OUTPUT_ARCH( "riscv" )
+        ENTRY(_start)
+        SECTIONS
+        {
+            . = 0x00000000;
+            .text : { *(.text) }
+            .data : { *(.data) }
+            .bss : { *(.bss) }
+        }
+        '''
+
+    mem = build_target(files)
+    mem = re.findall(r"\b[0-9A-Z]{2}\b", mem)
+    mem = bytes([int(n, 16) for n in mem])
+    mem = unpack("<{}I".format(len(mem)//4), mem)
+    mem = {k: v for k, v in enumerate(mem)}
 
     fork(exec_rom(dut.clk, dut.o_ibus_adr, dut.o_ibus_cyc,
                   dut.i_ibus_rdt, dut.i_ibus_ack, mem))
