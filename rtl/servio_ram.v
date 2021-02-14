da3ef6dfdbaccae0a3140394f73d41ccead9b9d7
yitiandelan <yitiandelan@outlook.com>
2021-02-14 19:32:41 +0800

    fix: port rom if to avs

--- rtl/servio_ram.v
+++ rtl/servio_ram.v
@@ -1,21 +1,25 @@
+// Copyright (c) 2021 TIANLAN.tech
+// SPDX-License-Identifier: Apache-2.0
+
+// Language: Verilog 2001
+
 `timescale 1ps / 1ps
 
 module servio_rom #(
     parameter DATA_DEPTH = 1024,
     parameter DATA_WIDTH = 8
 )(
-    input wire [aw-1:0] wb_s0_adr,
-    input wire          wb_s0_cyc,
-    output reg [dw-1:0] wb_s0_rdt,
-    output reg          wb_s0_ack,
+    input wire [aw-1:0] avs_s0_address,
+    input wire          avs_s0_read,
+    output reg [dw-1:0] avs_s0_readdata,
+    output reg          avs_s0_readdatavalid,
 
-    input wire [aw-1:0] wb_s1_adr,
-    input wire          wb_s1_cyc,
-    input wire          wb_s1_we,
-    input wire [dw-1:0] wb_s1_dat,
+    input wire [aw-1:0] avs_s1_address,
+    input wire          avs_s1_write,
+    input wire [dw-1:0] avs_s1_writedata,
 
-    input  wire         clk,
-    input  wire         reset
+    input wire          clk,
+    input wire          reset
 );
 
     localparam aw = $clog2(DATA_DEPTH);
@@ -25,21 +29,21 @@ module servio_rom #(
     reg  [aw-1:0] raddr, waddr;
 
     always @(*) begin
-        raddr = wb_s0_adr;
-        waddr = wb_s1_adr;
+        raddr = avs_s0_address;
+        waddr = avs_s1_address;
     end
 
     always @(posedge clk) begin
-        if (wb_s0_cyc) begin
-            wb_s0_rdt <= mem[raddr];
-            wb_s0_ack <= 1;
+        if (avs_s0_read) begin
+            avs_s0_readdata      <= mem[raddr];
+            avs_s0_readdatavalid <= 1;
         end
         else begin
-            wb_s0_ack <= 0;
+            avs_s0_readdatavalid <= 0;
         end
 
-        if (wb_s1_cyc & wb_s1_we) begin
-            mem[waddr] <= wb_s1_dat;
+        if (avs_s1_write) begin
+            mem[waddr] <= avs_s1_writedata;
         end
     end
 
